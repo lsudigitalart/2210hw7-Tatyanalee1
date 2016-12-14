11ee21cfd8bb6086f5ee00e2bf02ab11b5d2de83
Tatyana Lee <Tatyana Lee>
2016-12-13 18:22:28 -0600

    music

--- Sketch.js
+++ Sketch.js
@@ -1,13 +1,21 @@
+var song;
+
+function preload() {
+  song = loadSound('Fun_cut.mp3');
+}
+
 function setup () {
-  createCanvas(400,400);
+  createCanvas(500,500);
+  song.play();
+  background (0);
 }
 
 function draw() {
   background(204);
-  for (var x = 20; x < width; x += 20) {
-    var mx = mouseX / 10;
-    var offsetA = random(-mx, mx);
-    var offsetB = random(-mx, mx);
-    line(x + offsetA, 20, x - offsetB, 100);
+  //for (var x = 20; x < width; x += 20) {
+  //  var mx = mouseX / 10;
+  //  var offsetA = random(-mx, mx);
+  //  var offsetB = random(-mx, mx);
+  //  line(x + offsetA, 20, x - offsetB, 100);
   }
 }
